ad5667d87e997f65ba23e437fef2c0155f91f82c
DVLazarev <d.v.lazaryev@student.khai.edu>
2021-09-16 13:00:13 +0300

    change test.py

--- test.py
+++ test.py
@@ -2,4 +2,9 @@
 print("Python")
 =======
 print("educathion")
+<<<<<<< HEAD
 >>>>>>> eed8eba (test.py added to feature_branch with change on string#2)
+=======
+# new string
+print("DICT")
+>>>>>>> 3351a4f (test.py added to feature_branch with change on string#3)
