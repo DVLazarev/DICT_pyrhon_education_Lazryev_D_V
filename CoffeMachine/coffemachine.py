ba1733555c4efb36f25550f371afdf0248935344
DVLazarev <d.v.lazaryev@student.khai.edu>
2021-10-26 15:34:49 +0300

    CoffeMachine 3-rd stage

--- CoffeMachine/coffemachine.py
+++ CoffeMachine/coffemachine.py
@@ -1,10 +1,18 @@
-print("How many cups coffee do you want?")
-c = int(input("> "))
-w = 200
-m = 50
-b = 15
-print("For ", c ,"cups of coffee need:")
-print(c * w, "ml watter")
-print(c * m, "ml milk")
-print(c * b, "g of coffee beans")
+def cof():
+    w = int(input("Please how many water has coffee machine: > "))
+    m = int(input("Please how many milk has coffee machine: > "))
+    b = int(input("Please how many beans has coffee machine: > "))
+    c = int(input("Please how many cups you need: > "))
+    water = w // 200
+    milk = m // 50
+    beans = b // 15
+    cup = [water, milk, beans]
+    cup2 = min(cup)
+    if cup2 == c:
+        print("Yes i can make coffee for you!")
+    elif cup2 > c:
+        print("Yes i can make coffee for you (and even " + str(cup2) + "more than that)")
+    else:
+        print("No, i can make only " + str(cup2) +"cups of coffee")
+cof()
 
