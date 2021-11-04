a39b83b429f0b9a0d231951911c392ede8471bf1
DVLazarev <d.v.lazaryev@student.khai.edu>
2021-11-04 13:47:54 +0300

    Hangman 7 stage

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -16,6 +16,38 @@ while turns > 0:
            missed= missed + 1
 
      print
+     letter = []
+     wrong_letter = []
+
+
+
+     def de_sh():
+         for letter1 in secret:
+             if letter1 in letter:
+                 print(letter1, end='')
+             else:
+                 print('_', end='')
+                 if letter1 in letter:
+                     print("You've already guessed this letter.")
+                     if letter1 in wrong_letter:
+                         print("You've already guessed this letter.")
+
+
+     def playing():
+         life = 8
+         while life > 0:
+             print('')
+             letter1 = input("Input a letter: ")
+             if len(letter1) == 0 or len(letter1) > 1:
+                 print('You should input a single letter')
+                 print('')
+                 de_sh()
+                 continue
+             if letter1.capitalize() == letter1:
+                 print('Please enter a lowercase English letter')
+                 print('')
+                 de_sh()
+                 continue
 
      if missed == 0:
          print ('\nYou win!')
@@ -37,7 +69,3 @@ while turns > 0:
              print ('\n\nThe answer is', secret)
 
 
-
-
-
-print("Thanks for playing!")
