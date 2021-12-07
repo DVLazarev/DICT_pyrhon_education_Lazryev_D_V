44f2d5fa6cf21649860a764128007393b3708346
DVLazarev <d.v.lazaryev@student.khai.edu>
2021-12-07 17:25:54 +0300

    MatrixCalc v2 3 stage

--- MatrixProc/MartixProc.py
+++ MatrixProc/MartixProc.py
@@ -1,44 +1,110 @@
+# Matrix 2.0
 
-
+from colorama import Fore, Back, Style
 from colorama import init
-from colorama import Back, Fore, Style
-
 init()
 
+print(Back.GREEN + Fore.BLACK + Style.BRIGHT)
+
+def matrix_input(row):
+    array_row = []
+    for i in range(row):
+        array_col = []
+        for j in input().split():
+            if j != " ":
+                array_col.append(float(j))
+        array_row.append(array_col)
+    return array_row
 
 
-def it_m():
-    print( Back.RED + Fore.BLACK + Style.BRIGHT)
-    measurement = input("Введите кол-во строк и столбов: ").split()
-    n, m = measurement
-    mx = []
-    for y in range(int(n)):
-        mx.append(input().split())
-    return mx
+def add_matrices(m1, m2, row, col):
+    matrix_sum = []
+    for i in range(row):
+        temp = []
+        for j in range(col):
+            temp.append(m1[i][j] + m2[i][j])
+        matrix_sum.append(temp)
+    return matrix_sum
+
+
+def matrix_print(result, row, col):
+    for i in range(row):
+        for j in range(col):
+            print(result[i][j], end=' ')
+        print()
+
+
+def multiply_matrix_constant(m1, matrix2, row, col):
+    matrix_sum = []
+    for i in range(row):
+        temp = []
+        for j in range(col):
+            temp.append(m1[i][j] * matrix2)
+        matrix_sum.append(temp)
+    return matrix_sum
+
+
+def matrix_element_sum(l1, l2):
+    sum = 0
+    for i in range(len(l1)):
+        sum += l1[i] * l2[i]
+    return sum
+
+
+def matrix_two_multiply(m1, m2):
+    c = [[0 for row in range(len(m2[0]))] for col in range(len(m1))]
+    for i in range(len(m1)):
+        l1 = m1[i]
+        for j in range(len(m2[0])):
+            l2 = [m2[x][j] for x in range(len(m2))]
+            value = matrix_element_sum(l1, l2)
+            c[i][j] = value
+    return c
 
 
 
-def sum_mat():
 
+while True:
+    a = input("""1. Add matrices
+2. Multiply matrix by a constant
+3. Multiply matrices
+4. Exit 
+""")
+    if a == '1':
+        print(Back.CYAN + Fore.BLACK + Style.BRIGHT)
+        m1row, m1col = map(int, input("Enter size of first matrix:").split())
+        print('Enter first matrix:')
+        matrix1 = matrix_input(m1row)
+        m2row, m2col = map(int, input("Enter size of second matrix:").split())
+        print("Enter second matrix:")
+        matrix2 = matrix_input(m2row)
+        if m1row != m2row and m1col != m2col:
+            print('ERROR')
+        else:
+            matrix_result = add_matrices(matrix1, matrix2, m1row, m1col)
+            matrix_print(matrix_result, m1row, m1col)
+    elif a == '2':
+        print(Back.YELLOW + Fore.BLACK + Style.BRIGHT)
+        m1row, m1col = map(int, input("Enter size of matrix:").split())
+        print('Enter matrix:')
+        matrix1 = matrix_input(m1row)
+        print("Enter constant:")
+        matrix2 = float(input())
+        matrix_result = multiply_matrix_constant(matrix1, matrix2, m1row, m1col)
+        matrix_print(matrix_result, m1row, m1col)
+    elif a == '3':
+        print(Back.BLUE + Fore.BLACK + Style.BRIGHT)
+        m1row, m1col = map(int, input('Enter size of first matrix:').split())
+        print('Enter first matrix:')
+        matrix1 = matrix_input(m1row)
+        m2row, m2col = map(int, input('Enter size of second matrix:').split())
+        print('Enter second matrix:')
+        matrix2 = matrix_input(m2row)
 
-    mtx = it_m()
-    mtx_2 = it_m()
-    if len(mtx) == len(mtx_2) and len(mtx[0]) == len(mtx_2[0]):
-        for i in range(len(mtx)):
-            for x in range(len(mtx[0])):
-                print(float(mtx[i][x]) + float(mtx_2[i][x]), end=' ')
-            print('')
-    else:
-        print("ERROR")
+        matrix_result = matrix_two_multiply(matrix1, matrix2)
+        print('The result is:')
+        matrix_print(matrix_result, m1row, m2col)
+    elif a == '4':
+        print(Style.RESET_ALL)
 
-def multiplication():
-
-
-    mtx = it_m()
-    const = input("Введите константу: ")
-    for i in range(len(mtx)):
-        for x in range(len(mtx[0])):
-            print(float(mtx[i][x]) * float(const), end=' ')
-        print('')
-
-multiplication()
+        break
